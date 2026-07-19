dfb4a15b75e697713282517436268ee6ae87645a
muralivmuraliv89@gmail.com <muralivmuraliv89@gmail.com>
2026-07-19 12:35:53 +0530

    1st file

---
+++ 1.py
@@ -0,0 +1,22 @@
+#printing statements
+print("hello world!")
+print("This is \'a \"test script.\"\'\n")
+
+#comments
+#single line comment
+'''multi line comment
+This is a multi line comment'''
+"""this is also a multi line comment"""
+
+#variables and data types
+x = 5 #integer
+y = 3.14 #float
+ch = 'a' #character
+name = "John" #string
+
+print("Integer:", x)
+print("Float:", y)
+print("Character:", ch)
+print("String:", name)
+print("Type of x:", type(x))
+print("sum of x & y:", x+y)
