518aebfdc76d4240f32d0e1a5932a8da3149d150
muralivmuraliv89@gmail.com <muralivmuraliv89@gmail.com>
2026-07-31 21:14:44 +0530

    implementing functions

---
+++ functions.py
@@ -0,0 +1,21 @@
+#Write a function greet() that takes no 
+# arguments and prints a greeting message.
+def greet():
+    print("Hello Brooooo")
+
+greet()
+
+#  Write a function greet_user() that takes a name as input and prints a custom greeting.
+def greet_user(name):
+    print(f"Heloo {name} brooo")
+
+greet_user("Murali")
+
+#Write a function add_numbers(a, b) that returns the sum of two numbers. Call this function with different values.
+def add(n1,n2):
+    return n1+n2
+
+add(2,3)
+add(3,20)
+add(7,2)
+add(9,3)
