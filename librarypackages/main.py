6b840a8a3747bd086a73f915afe8a92c079e2a1e
muralivmuraliv89@gmail.com <muralivmuraliv89@gmail.com>
2026-08-23 21:40:38 +0530

    Libraries & Packages & Module

---
+++ librarypackages/main.py
@@ -0,0 +1,16 @@
+import random
+from math import sqrt
+from package1 import greeting
+
+
+print("Hello world")
+greeting.greet("murali")
+
+
+print(sqrt(36))  # No need to write math.sqrt()
+
+
+print(random.randint(1, 10))  # Random number between 1 and 10
+import wikipedia
+
+print(wikipedia.summary("Virat Kohli"))
