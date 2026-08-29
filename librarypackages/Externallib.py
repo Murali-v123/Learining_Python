ad07b2817996f0a24564380c66bb2dfd279e0f46
muralivmuraliv89@gmail.com <muralivmuraliv89@gmail.com>
2026-08-29 20:55:37 +0530

    updated code

--- librarypackages/Externallib.py
+++ librarypackages/Externallib.py
@@ -1,16 +1,16 @@
 # usage of external libraries
-from currency_converter import CurrencyConverter 
+from currency_converter import CurrencyConverter
 from datetime import datetime
 from datetime import date
 
-c=CurrencyConverter()
+c = CurrencyConverter()
 
-c.convert(112,'EUR','USD')
+c.convert(112, "EUR", "USD")
 
 # to convert user entered amount to inr
-inr=int(input("Enter any amount:"))
-amt=c.convert(inr,"INR","USD")
+inr = int(input("Enter any amount:"))
+amt = c.convert(inr, "INR", "USD")
 print(f"The amount in usd is {amt}")
-print(datetime.now()) 
-print(datetime.now().time()) 
-print(date.today()) 
+print(datetime.now())
+print(datetime.now().time())
+print(date.today())
