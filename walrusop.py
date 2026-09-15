cb5ed2cae90b8844003f9484f3b097d3c8ee998a
muralivmuraliv89@gmail.com <muralivmuraliv89@gmail.com>
2026-09-15 22:09:52 +0530

    walrus operator

---
+++ walrusop.py
@@ -0,0 +1,15 @@
+# walrus operator :=
+# a=10
+# print(a)
+# print(a:=20)
+# a=[11,2,3,4,5]
+
+# while(n:=len(a))>0:
+#     print(n)
+#     a.pop()
+
+foods=[]
+while(food:= input("Enter a number"))!="quit":
+    foods.append(food)
+
+print(foods)
